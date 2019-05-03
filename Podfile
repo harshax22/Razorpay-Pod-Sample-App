6a78c98972bf8f4cdddfb856e3ea40d488ac4cef
Harsha Marri <harshamarri@cmp-249.biz.razorpay.com>
2019-05-03 16:36:38 +0530

    first commit

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RazorpayPod Sample App' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'razorpay-pod', '1.1.2'
+
+  # Pods for RazorpayPod Sample App
+
+  target 'RazorpayPod Sample AppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'RazorpayPod Sample AppUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- RazorpayPod Sample App/ViewController.swift
+++ RazorpayPod Sample App/ViewController.swift
@@ -7,14 +7,66 @@
 //
 
 import UIKit
+import Razorpay
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , RazorpayPaymentCompletionProtocol {
+    func onPaymentError(_ code: Int32, description str: String) {
+      
+        let alert = UIAlertController(title: "Failure!", message: "Your payment is failed", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+            NSLog("The \"OK\" alert occured.")
+        }))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    func onPaymentSuccess(_ payment_id: String) {
+        
+        let alert = UIAlertController(title: "Success!", message: "Payment is \(payment_id)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+            NSLog("The \"OK\" alert occured.")
+        }))
+        self.present(alert, animated: true, completion: nil)
+
+    }
+    
+    
+    private var razorpay: Razorpay!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        razorpay = Razorpay.initWithKey("rzp_test_IFMmoKlMshyTeL", andDelegate: self)
+        
+    }
+    
+    private func openCheckout() {
+        let options: [String: Any] = [
+            "amount" : "10000",
+            "description": "Stranger Things",
+            "image": "https://venturebeat.com/wp-content/uploads/2016/06/netflix-logo.png?w=1200&strip=all",
+            "name": "Netflix",
+            "currency" : "INR",
+            "prefill": [
+             "contact": "7019284852",
+             "email": "harsha.marri@razorpay.com"
+             ],
+            "notes": [
+                "key1":"45678.0000"
+            ],
+            "external" : [
+                "wallets" : [ "jiomoney" ]
+            ],
+
+            "theme": ["color": "#000000"]
+        ]
+        razorpay.open(options, displayController: self)
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        openCheckout()
+        }
+    
 }
 
